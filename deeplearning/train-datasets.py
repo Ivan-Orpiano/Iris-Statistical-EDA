8c6e5fe6e80a588f97c74dd0579fbfe8ed30318a
Ivan Ritchel Orpiano <147624545+Ivan-Orpiano@users.noreply.github.com>
2026-08-04 20:37:42 +0800

    s

--- deeplearning/train-datasets.py
+++ deeplearning/train-datasets.py
@@ -19,3 +19,11 @@ transform_test = transforms.Compose([
     
 ])
 
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+#load CIFAR-10 dataset
+train_dataset = datasets.CIFAR10(root = "./data", train = True, download = True, transforms = transform_train)
