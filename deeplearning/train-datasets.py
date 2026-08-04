7247b10cbc1447aacfb27032dd4911f8bc7b5feb
Ivan Ritchel Orpiano <147624545+Ivan-Orpiano@users.noreply.github.com>
2026-08-04 20:20:58 +0800

    S

---
+++ deeplearning/train-datasets.py
@@ -0,0 +1,10 @@
+#Training datasets using pytorch
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+
