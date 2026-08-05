d87960be79cbbb7948b33ab558321ef2127a14e1
Ivan Ritchel Orpiano <147624545+Ivan-Orpiano@users.noreply.github.com>
2026-08-05 15:25:10 +0800

    s

--- deeplearning/train-datasets.py
+++ deeplearning/train-datasets.py
@@ -35,7 +35,18 @@ test_loader = DataLoader(test_dataset, batch_size=64, shuffle = False)
 print(f"Training Data Size: {len(train_dataset)}")
 print(f"Test Data Size: {len(test_dataset)}")
 
-
+class EnhancedCNN (nn.Module):
+    def __init__(self):
+        super(EnhancedCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3,6,5)
+        self.bn1 = nn.BatchNorm2d(6)
+        self.conv2 = nn.Conv2d(6,16,5)
+        self.bn2 = nn.BatchNorm2d(16)
+        self.pool = nn.MaxPool2d(2,2)
+        self.dropout = nn.Dropout(0,5)
+        
+        #calculate the size of the output
+        self._calculate_conv_output()
 
 
 
