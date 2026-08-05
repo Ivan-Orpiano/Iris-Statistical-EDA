5b59eb93c6bacc1f65ad05747ab89d7ba0e2bcea
Ivan Ritchel Orpiano <147624545+Ivan-Orpiano@users.noreply.github.com>
2026-08-05 21:56:22 +0800

    s

--- deeplearning/train-datasets.py
+++ deeplearning/train-datasets.py
@@ -63,7 +63,14 @@ class EnhancedCNN (nn.Module):
         x = F.relu(self.bn1(self.conv1(x)))
         x = self.pool(F.relu(self.bn2(self.conv2(x))))
         x = x.view(x.size(0), -1)
-            
+        x =  F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+    
+model = EnhancedCNN()
+print(model)
             
             
             
