100459d798d62579f9fcf1367c3cc4c9594dbffc
Ivan Ritchel Orpiano <147624545+Ivan-Orpiano@users.noreply.github.com>
2026-08-07 12:29:07 +0800

    s

--- deeplearning/train-datasets.py
+++ deeplearning/train-datasets.py
@@ -103,6 +103,29 @@ def evaluate_model(model, test_loader):
     total = 0
     with torch.no_grad():
         for images, labels in test_loader:
-            pass
+            outputs = model(images)
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+        print(f"Test Accuracy: {100 * correct / total:.2f}%")
             
-            
+evaluate_model(model, test_loader)
+
+
+import matplotlib.pylab as plt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
